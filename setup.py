643ee74da2725f02af68e719507e72b588529105
Aeron Chamara <achamara@bglcorp.com.au>
2017-02-02 15:43:26 +1100

    setup - installation

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(name='DocManagementScripts',
+      version='0.1',
+      description='Combination of scripts to manage documentary content.',
+      url='https://github.com/compilable/',
+      author='achamara',
+      author_email='achamara@gmail.com',
+      license='MIT',
+      packages=['docDuplicateManager'],
+      scripts=['bin/findDuplicates'],
+      install_requires=[
+          'TinyDB'   ],
+      include_package_data=True,
+      zip_safe=False)
+
+
+'''
+
+http://python-packaging.readthedocs.io/en/latest/non-code-files.html
+
+To Install :
+
+Run below command from DocManagementScripts folder
+
+sudo pip install -e .   
+
+To Execute : 
+
+DocManagementScripts config.ini
+
+'''
